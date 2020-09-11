ed9fd0bbe952d17eaa347c8494ec62b9dc5bcb0d
jbnilles <jbnilles24@gmail.com>
2020-09-11 08:32:55 -0700

    add currencyExchange.js and initial code for api call

---
+++ src/currencyExhange.js
@@ -0,0 +1,18 @@
+export default class CurrencyExchange {
+  static getRates(country) {
+    return new Promise(function (resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${country}`;
+      request.onload = function() {
+        if(this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open('GET', url, true);
+      request.send();
+
+    });
+  }
+}
