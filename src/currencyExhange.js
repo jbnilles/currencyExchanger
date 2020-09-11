29d0695bde3bde90b7190ba43e94ab188e4571b8
jbnilles <jbnilles24@gmail.com>
2020-09-11 14:33:37 -0700

    add notification for missing currency

--- src/currencyExhange.js
+++ src/currencyExhange.js
@@ -3,15 +3,12 @@ export default class CurrencyExchange {
   static getRates(country) {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEYy}/latest/${country}`;
+      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${country}`;
       request.onload = function() {
         if(this.status === 200) {
-          console.log(this)
           resolve(request.response);
         } else {
-          alert('herr')
           reject(request.response);
-          
         }
       };
       request.open('GET', url, true);
@@ -20,25 +17,23 @@ export default class CurrencyExchange {
     });
   }
   static writeRatesToSession() {
-    let promise = CurrencyExchange.getRates('USDD');
+    let promise = CurrencyExchange.getRates('USD');
     promise.then(function (response) {
       const body = JSON.parse(response);
-      console.log(body);
       if(body.result === 'success'){
       const conversionKeys = Object.keys(body.conversion_rates);
       conversionKeys.forEach(function (key){
         sessionStorage.setItem(key, body.conversion_rates[key]);
       })
+    } else {
+      alert('The currency code selected is not supported in this application');
     }
-      //console.log(sessionStorage);
     }, function(error) {
-    console.log(error);
+      console.log(error);
     });
   }
 
   static convert(amountFrom,countryFrom, countryTo) {
-    console.log(sessionStorage.getItem(countryFrom) / sessionStorage.getItem(countryTo))
-    console.log( sessionStorage.getItem(countryTo) / sessionStorage.getItem(countryFrom))
     return (amountFrom / sessionStorage.getItem(countryFrom)) * sessionStorage.getItem(countryTo);
   }
 
